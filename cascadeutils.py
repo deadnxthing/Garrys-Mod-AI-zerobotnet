6088ab20f10fa5b5b1fa74cfbf94dd2a3d0f4e94
kuba <119539361+deadnxthing@users.noreply.github.com>
2022-12-01 05:34:21 +0100

    Update cascadeutils.py

--- cascadeutils.py
+++ cascadeutils.py
@@ -8,8 +8,5 @@ def genereate_negative_description_file():
 genereate_negative_description_file()
 
 
-# $ C:/Users/Ben/learncodebygaming/opencv/build/x64/vc15/bin/opencv_traincascade.exe -data cascade/ -vec pos.vec -bg neg.txt -precalcValBufSize 6000 -precalcIdxBufSize 6000 -numPos 1100 -numNeg 3300 -numStages 12 -w 24 -h 24 -maxFalseAlarmRate 0.4 -minHitRate 0.999
-#-data sztuczna/ -vec pos.vec -bg neg.txt -precalcValBufSize 6000 -precalcIdxBufSize 6000 -numPos 1000 -numNeg 3000 -numStages 20 -w 24 -h 24 -maxFalseAlarmRate 0.4 -minHitRate 0.999
-
 
 #C:\Users\KUBAC\Downloads\opencv\build\x64\vc15\bin
